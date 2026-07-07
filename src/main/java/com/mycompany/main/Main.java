45b372dea9bee81f167f4f541ff00d6790d1939e
Nguyễn Kim Hiếu <sandpassed15@gmail.com>
2026-07-07 08:44:51 +0700

    Initial Java project

---
+++ src/main/java/com/mycompany/main/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.main;
+
+/**
+ *
+ * @author DELL
+ */
+public class Main {
+public static void main(String[] args) {
+        System.out.println("Chào Nguyễn Kim Hiếu");
+    }
+}

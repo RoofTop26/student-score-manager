4391a6eddbcc1c9308ea585eaa3224daca3e2076
Nguyễn Kim Hiếu <sandpassed15@gmail.com>
2026-07-07 16:36:12 +0700

    Day 2: student

--- src/main/java/com/mycompany/main/Main.java
+++ src/main/java/com/mycompany/main/Main.java
@@ -10,6 +10,24 @@ package com.mycompany.main;
  */
 public class Main {
 public static void main(String[] args) {
-        System.out.println("Hello Nguyễn Kim Hiếu");
+        Student student = new Student();
+
+        student.name = "Nguyen Kim Hieu";
+        student.studentId = 123456;
+
+        student.scores = new double[]{6, 7, 9.5};
+
+        double average = (student.scores[0] + student.scores[1] + student.scores[2]) / 3;
+
+        int averageInt = (int) average;
+        final double DIEM_DAT = 5.0;
+        student.isPassed = average >= DIEM_DAT;
+
+        System.out.println("Ten: " + student.name);
+        System.out.println("Ma SV: " + student.studentId);
+        System.out.println("Diem TB: " + average);
+        System.out.println("So sanh ep: " + averageInt);
+        System.out.println("Dat: " + student.isPassed);
+
     }
 }
